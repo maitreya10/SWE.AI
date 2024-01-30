d6a9f67337ccc6599859a482dfcb4f4a024fce06
maitreya10 <59189494+maitreya10@users.noreply.github.com>
2024-01-29 22:48:56 -0500

    Create Best Time to Buy and Sell Stock.java
    Done by Two pointers (Sliding Window)
    TC: O(n)

---
+++ Best Time to Buy and Sell Stock.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        if (prices.length == 1) {
+            return 0;
+        }
+        int left = 0;
+        int right = 1;
+        int max = prices[right] - prices[left];
+        while (right < prices.length) {
+            if (prices[left] > prices[right]) {
+                left++;
+            } else {
+                int diff = prices[right] - prices[left];
+                if (diff > max) {
+                    max = diff;
+                }
+                right++;
+            }
+        }
+        return max;
+    }
+}
