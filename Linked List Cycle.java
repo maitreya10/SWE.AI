1563ccb9ad2868493f7f26930e62b9797cf2ac5e
maitreya10 <59189494+maitreya10@users.noreply.github.com>
2024-03-06 10:57:22 -0800

    Create Linked List Cycle.java
    T.C: O(n)
    S.C : O(1)

---
+++ Linked List Cycle.java
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        // Floyd's Tortoise and hare Technique.
+        // Fast moves by 2, slow moves by 1 : If a cycle exists, Both pointers are guaranteed to meet. (See notes for proof).
+        ListNode fast = head;
+        ListNode slow = head;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if (fast == slow) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
