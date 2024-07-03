26fad17a97d1db1a85db6fa668276a7a90982a27
maitreya10 <59189494+maitreya10@users.noreply.github.com>
2024-07-03 09:40:09 +0530

    Create Climbing Stairs.java

---
+++ Climbing Stairs.java
@@ -0,0 +1,50 @@
+class Solution {
+    public int climbStairs(int n) {
+        int left = 1;
+        int right = 1;
+        for (int i = 0; i < n - 1; i++) {
+            int tmp = left;
+            left = left + right; // see diagram in the book.
+            right = tmp ;
+        }
+        return left;
+    }
+}
+/*
+//bottom up
+class Solution {
+
+    public int climbStairs(int n) {
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = 1;
+
+        for (int i = 2; i < n + 1; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n];
+    }
+}
+
+//top down with memo[]
+class Solution {
+
+    public int climbStairs(int n) {
+        int[] memo = new int[n + 1];
+        Arrays.fill(memo, -1);
+
+        return climbStairs(n - 1, memo) + climbStairs(n - 2, memo);
+    }
+
+    private int climbStairs(int n, int[] memo) {
+        if (n < 0) return 0;
+        if (n == 0 || n == 1) {
+            memo[n] = 1;
+            return memo[n];
+        }
+        if (memo[n] != -1) return memo[n];
+
+        memo[n] = climbStairs(n - 1, memo) + climbStairs(n - 2, memo);
+        return memo[n];
+    }
+*/
